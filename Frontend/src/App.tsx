ee27e2fe7e02ec0758183b795f3879f7866c8ad9
Yohanes Senbeto Kankure <121575345+YohanesSenbeto@users.noreply.github.com>
2024-05-20 21:12:52 +0000

    :sparkles: New signup component created

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,28 +1,18 @@
-import Navbar from './components/Navbar';
-import { BrowserRouter, Routes } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 function App() {
     return (
-        
         <BrowserRouter>
             <Navbar />
             <Routes>
-
-                {"other routes go here"}
-
-                
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/Login" element={<Login />} />
+                {/* other routes go here */}
             </Routes>
-            
-            
         </BrowserRouter>
-      
     );
 }
 
 export default App;
-
-
-
-
-
-
