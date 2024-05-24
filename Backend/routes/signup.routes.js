e3f1b892ab9fe31b0b96f686957283bbdff72246
Eyob Sisay <ehtube2020@gmail.com>
2024-05-24 21:57:32 +0300

    🔧 chore: update backend signup routes

--- Backend/routes/signup.routes.js
+++ Backend/routes/signup.routes.js
@@ -2,11 +2,10 @@
 const express = require("express");
 // Call the router method from express to create the router
 const router = express.Router();
-// Import the sign-up controller
-const signUpControllers = require("../controllers/signup.controller");
+// Import the login controller
+const signupController = require("../controllers/user.controller");
+// Create a route to handle the login request on post
 
-// Route for normal, admin, manager, and staff users
-router.post("/api/signup", signUpControllers.signup);
-
+router.post("/api/signup", signupController.createUser);
 // Export the router
 module.exports = router;
