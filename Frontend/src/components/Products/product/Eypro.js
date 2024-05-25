97ebaef1b250a2c44c05077c1d46f162a0d9589d
Eyob Sisay <ehtube2020@gmail.com>
2024-05-25 13:52:12 +0000

    Sat, May 25, 2024, 4:52 PM +03:00

--- Frontend/src/components/Products/product/Eypro.js
+++ Frontend/src/components/Products/product/Eypro.js
@@ -1,1 +1,13 @@
-rfce
+import React from 'react'
+
+import Products from './Products'
+
+function Eypro() {
+    return (
+        <div>
+<Products/>
+        </div>
+    )
+}
+
+export default Eypro
