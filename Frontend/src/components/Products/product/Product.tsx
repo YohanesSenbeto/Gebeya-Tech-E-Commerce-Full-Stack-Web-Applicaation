e0e66989c92bf68252a13d30b05ca18977f365c1
Eyob Sisay <ehtube2020@gmail.com>
2024-05-25 16:49:51 +0000

    Sat, May 25, 2024, 7:49 PM +03:00

--- Frontend/src/components/Products/product/Product.tsx
+++ Frontend/src/components/Products/product/Product.tsx
@@ -47,6 +47,9 @@ const Product = ({ product, addToCart, loadCurrentItem }) => {
                 className="bg-[#FBBF24] text-[#0B2D5F] hover:bg-[#0B2D5F] hover:text-[#FBBF24] transition-colors"
                 size="sm"
               >
+                  </Button>
+                  <Button className="bg-[#FBBF24] text-[#0B2D5F] hover:bg-[#0B2D5F] hover:text-[#FBBF24] transition-colors"
+                size="sm">
                 <Link
                   to={`/product/${product.id}`}
                   className="inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#0B2D5F] disabled:pointer-events-none disabled:opacity-50"
