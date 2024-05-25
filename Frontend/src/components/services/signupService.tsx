087d8e0aec4c7ddfe20ecbb96ba840fa7bb593e3
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-25 21:11:23 +0300

    ♻️ refactor(signup): refactor signup service in frontend

--- Frontend/src/components/services/signupService.tsx
+++ Frontend/src/components/services/signupService.tsx
@@ -6,22 +6,21 @@ export interface FormData {
     confirmPassword: string;
     phone: string;
     gender: string;
-    user_role_id:number;
-    active_user:number;
-  }
-  
-  export async function signup(formData: FormData): Promise<void> {
-    const response = await fetch('http://localhost:8000/api/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
+    user_role_id: number;
+    active_user: number;
+}
+
+export async function signup(formData: FormData): Promise<void> {
+    const response = await fetch("http://44.219.138.130:8000/api/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
     });
-  
+
     if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(errorData.message || 'Failed to sign up');
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Failed to sign up");
     }
-  }
-  
+}
