fc7dd14f0db425e2bb90495a2329f4686c683ad9
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-25 21:11:15 +0300

    ♻️ refactor(login): refactor login service in frontend

--- Frontend/src/components/services/loginservice.tsx
+++ Frontend/src/components/services/loginservice.tsx
@@ -1,51 +1,43 @@
-const api_url = `http://localhost:8000`; 
+const api_url = `http://44.219.138.130:8000`;
 
 interface LoginForm {
-  email:string;
-  password: string;
-  
+    email: string;
+    password: string;
 }
 
-// A function to send the login request to the server 
+// A function to send the login request to the server
 const logIn = async (formData: LoginForm): Promise<Response> => {
-  const requestOptions: RequestInit = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(formData),
-  };
-  console.log("About to send request");
-  console.log(requestOptions.body);
-  const response = await fetch(`${api_url}/api/login`, requestOptions);
+    const requestOptions: RequestInit = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+    };
+    console.log("About to send request");
+    console.log(requestOptions.body);
+    const response = await fetch(`${api_url}/api/login`, requestOptions);
 
-  const data = await response.json();
-  console.log(data)
-  
+    const data = await response.json();
+    console.log(data);
 
+    if (data.status === "success") {
+        const token = data.data.user_token;
+        const user = data.data.user;
 
-  if (data.status === "success") {
-    const token = data.data.user_token;
-    const user = data.data.user;
+        // Store the token and user data (e.g., in localStorage or state management)
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
 
-    // Store the token and user data (e.g., in localStorage or state management)
-    localStorage.setItem('token', token);
-    localStorage.setItem('user', JSON.stringify(user));
-
-    console.log('User logged in successfully', user);
-  } else {
-    console.error('Login failed:', data.message);
-  }
-  return response;
-  
-}
+        console.log("User logged in successfully", user);
+    } else {
+        console.error("Login failed:", data.message);
+    }
+    return response;
+};
 
 // A function to log out the user
 const logOut = (): void => {
-  localStorage.removeItem("user");
-
+    localStorage.removeItem("user");
 };
-export default logOut
-// Export the functions 
-export {
-  logIn
-  
-}
+export default logOut;
+// Export the functions
+export { logIn };
