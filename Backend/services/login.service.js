c0cea1a56cbec9f491a29f836f9c2aa7eb66bb7f
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-25 21:10:40 +0300

    ✨ feat(login): enhance login service logic

--- Backend/services/login.service.js
+++ Backend/services/login.service.js
@@ -1,43 +1,46 @@
-// Import the query function from the db.config.js file 
+// Import the query function from the db.config.js file
 const conn = require("../config/db.config");
-// Import the bcrypt module to do the password comparison 
-const bcrypt = require('bcrypt');
-// Import the user service to get user by email  
+// Import the bcrypt module to do the password comparison
+const bcrypt = require("bcryptjs");
+// Import the user service to get user by email
 const userService = require("./user.service");
-// Handle user login 
+// Handle user login
 async function logIn(userData) {
-  try {
-    let returnData = {}; // Object to be returned
-    const user = await userService.getUserByEmail(userData.email);
-    if (user.length === 0) {
-      returnData = {
-        status: "fail",
-        message: "user does not exist"
-      };
-      return returnData;
-    }
-    const passwordMatch = await bcrypt.compare(userData.password, user.password);
-    if (!passwordMatch) {
-      returnData = {
-        status: "fail",
-        message: "Incorrect password"
-      };
-      return returnData;
-    }
+    try {
+        let returnData = {}; // Object to be returned
+        const user = await userService.getUserByEmail(userData.email);
+        if (user.length === 0) {
+            returnData = {
+                status: "fail",
+                message: "user does not exist",
+            };
+            return returnData;
+        }
+        const passwordMatch = await bcrypt.compare(
+            userData.password,
+            user.password
+        );
+        if (!passwordMatch) {
+            returnData = {
+                status: "fail",
+                message: "Incorrect password",
+            };
+            return returnData;
+        }
 
-    console.log(user)
-    returnData = {
-      status: "success",
-      data: user
-    };
-    console.log(returnData)
-    return returnData;
-  } catch (error) {
-    console.log(error);
-  }
+        console.log(user);
+        returnData = {
+            status: "success",
+            data: user,
+        };
+        console.log(returnData);
+        return returnData;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-// Export the function 
+// Export the function
 module.exports = {
-  logIn
+    logIn,
 };
