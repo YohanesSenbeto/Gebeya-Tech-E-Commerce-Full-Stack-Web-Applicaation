7bee056fdf5e358fdf2d21e197280a26607ae9a9
Eyob Sisay <ehtube2020@gmail.com>
2024-05-25 13:30:59 +0000

    Sat, May 25, 2024, 4:30 PM +03:00

--- Frontend/src/components/Products/product/Product.js
+++ Frontend/src/components/Products/product/Product.js
@@ -11,18 +11,7 @@ import {
 
 const Product = ({ product, addToCart, loadCurrentItem }) => {
   return (
-    <div className={styles.product}>
-      <img
-        className={styles.product__image}
-        src={product.image}
-        alt={product.title}
-      />
-
-      <div className={styles.product__details}>
-        <p className={styles.details__title}>{product.title}</p>
-        <p className={styles.details__desc}>{product.description}</p>
-        <p className={styles.details__price}>$ {product.price}</p>
-      </div>
+   
 
       <div className={styles.product__buttons}>
         <Link to={`/product/${product.id}`}>
