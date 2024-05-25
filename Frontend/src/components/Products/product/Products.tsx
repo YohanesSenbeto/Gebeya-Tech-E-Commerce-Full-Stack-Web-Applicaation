6e818c9dca764f237ecd1d27073f6f2e13764d11
Eyob Sisay <ehtube2020@gmail.com>
2024-05-25 14:32:22 +0000

    Sat, May 25, 2024, 5:32 PM +03:00

--- Frontend/src/components/Products/product/Products.tsx
+++ Frontend/src/components/Products/product/Products.tsx
@@ -8,11 +8,16 @@ import {Product} from "./Product";
 
 const Products = ({ products }) => {
     return (
-      <div >
+<section className="bg-[#0B2D5F] text-white py-12">
+      <div className="container px-4 md:px-6 max-w-6xl mx-auto">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {products.map((product) => (
           <Product key={product.id} product={product} />
         ))}
+         </div>
       </div>
+    </section>
+
     );
   };
 
