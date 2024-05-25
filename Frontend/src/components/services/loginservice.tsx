d1b7d9163b55651faca571a3146abb8c935fb558
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-26 02:40:37 +0300

    🔄 refactor(frontend): refactor login service

--- Frontend/src/components/services/loginservice.tsx
+++ Frontend/src/components/services/loginservice.tsx
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-const api_url = `http://44.219.138.130:8000`;
+const api_url = `http://localhost:8000`;
 
 interface LoginForm {
     email: string;
     password: string;
-=======
-const api_url = `https://ec2-44-219-138-130.compute-1.amazonaws.com:8000/`;
-
-interface LoginForm {
-  email:string;
-  password: string;
-
->>>>>>> a4dabc894dffb76e8fc61fe314c7543ba31c3663
 }
-
 // A function to send the login request to the server
 const logIn = async (formData: LoginForm): Promise<Response> => {
     const requestOptions: RequestInit = {
@@ -24,15 +14,8 @@ const logIn = async (formData: LoginForm): Promise<Response> => {
     console.log("About to send request");
     console.log(requestOptions.body);
     const response = await fetch(`${api_url}/api/login`, requestOptions);
-
-<<<<<<< HEAD
     const data = await response.json();
     console.log(data);
-=======
-  const data = await response.json();
-  console.log(data)
-
->>>>>>> a4dabc894dffb76e8fc61fe314c7543ba31c3663
 
     if (data.status === "success") {
         const token = data.data.user_token;
@@ -41,41 +24,17 @@ const logIn = async (formData: LoginForm): Promise<Response> => {
         // Store the token and user data (e.g., in localStorage or state management)
         localStorage.setItem("token", token);
         localStorage.setItem("user", JSON.stringify(user));
-
-<<<<<<< HEAD
         console.log("User logged in successfully", user);
     } else {
         console.error("Login failed:", data.message);
     }
     return response;
 };
-=======
-    // Store the token and user data (e.g., in localStorage or state management)
-    localStorage.setItem('token', token);
-    localStorage.setItem('user', JSON.stringify(user));
-
-    console.log('User logged in successfully', user);
-  } else {
-    console.error('Login failed:', data.message);
-  }
-  return response;
-
-}
->>>>>>> a4dabc894dffb76e8fc61fe314c7543ba31c3663
 
 // A function to log out the user
 const logOut = (): void => {
     localStorage.removeItem("user");
 };
-<<<<<<< HEAD
 export default logOut;
 // Export the functions
 export { logIn };
-=======
-export default logOut
-// Export the functions
-export {
-  logIn
-
-}
->>>>>>> a4dabc894dffb76e8fc61fe314c7543ba31c3663
