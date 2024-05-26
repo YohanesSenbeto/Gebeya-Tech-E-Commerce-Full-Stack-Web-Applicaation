85f4bff174b9649cbea532e1cd859100d35c2e6c
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-26 18:56:01 +0300

    ✂️ Delete: Removed product routes

--- Backend/routes/product.Routes.js
+++
@@ -1,8 +0,0 @@
-// productRoutes.js
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/Product.Controller");
-
-router.get("/products/search", productController.searchProducts);
-
-module.exports = router;
