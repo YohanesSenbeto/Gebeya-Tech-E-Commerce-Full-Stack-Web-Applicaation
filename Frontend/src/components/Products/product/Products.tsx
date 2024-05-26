424dd798c2a1333876e71e696858769e0dcc26e8
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-26 18:57:53 +0300

    ℹ️ Stage and commit: Modified Products component

--- Frontend/src/components/Products/product/Products.tsx
+++ Frontend/src/components/Products/product/Products.tsx
@@ -1,5 +1,5 @@
+//products.tsx
 import React from "react";
-
 
 // Redux
 import { connect } from "react-redux";
@@ -8,27 +8,23 @@ import Product from "./Product";
 
 const Products = ({ products }) => {
     return (
-<section className="bg-[#0B2D5F] text-white py-12">
-      <div className="container px-4 md:px-6 max-w-6xl mx-auto">
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {products.map((product) => (
-          <Product key={product.id} product={product} />
-        ))}
-         </div>
-      </div>
-    </section>
-
+        <section className="bg-[#0B2D5F] text-white py-12">
+            <div className="container px-4 md:px-6 max-w-6xl mx-auto">
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {products.map((product) => (
+                        <Product key={product.id} product={product} />
+                    ))}
+                </div>
+            </div>
+        </section>
     );
-  };
-
-
-
+};
 
 const mapStateToProps = (state) => {
-  return {
-    products: state.shop.products,
-  };
+    return {
+        products: state.shop.products,
+    };
 };
 
 export default connect(mapStateToProps)(Products);
-export {Products}
+export { Products };
