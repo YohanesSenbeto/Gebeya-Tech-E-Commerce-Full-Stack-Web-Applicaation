d188e8143ce7b85a2ca861400d7f36ba3ef24811
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-26 18:18:24 +0300

    ℹ️ Stage and commit: Modified initial SQL queries

--- Backend/services/sql/initial-queries.sql
+++ Backend/services/sql/initial-queries.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
 
 -- Add order
 CREATE TABLE IF NOT EXISTS `orders` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT,
   `created_at` DATETIME,
   `total_amount` DECIMAL(10, 2),
@@ -37,16 +37,16 @@ CREATE TABLE IF NOT EXISTS `orders` (
 
 -- Add order_status
 CREATE TABLE IF NOT EXISTS `order_status` (
-  `order_status_id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) NOT NULL,
-  `order_status` int(11) NOT NULL,
+  `order_status_id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `order_status` INT NOT NULL,
   PRIMARY KEY (order_status_id),
   FOREIGN KEY (order_id) REFERENCES orders(order_id)
 ) ENGINE=InnoDB;
 
 --order items
 
-CREATE TABLE order_items (
+CREATE TABLE IF NOT EXISTS `order_items` (
   `order_item_id` INT PRIMARY KEY AUTO_INCREMENT,
   `order_id` INT,
   `product_id` INT,
@@ -54,25 +54,29 @@ CREATE TABLE order_items (
   `price` DECIMAL(10, 2),
   FOREIGN KEY (order_id) REFERENCES orders(order_id),
   FOREIGN KEY (product_id) REFERENCES products(product_id)
-)ENGINE=InnoDB;
+) ENGINE=InnoDB;
 
 --products
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS `products` (
   `product_id` INT PRIMARY KEY AUTO_INCREMENT,
   `product_name` VARCHAR(255),
   `description` TEXT,
+  `category` VARCHAR(100) NOT NULL,
+  `image_url` VARCHAR(255),
   `price` DECIMAL(10, 2) CHECK (price >= 0),
-  `stock` INT CHECK (stock >= 0)
-)ENGINE=InnoDB;
+  `stock` INT CHECK (stock >= 0),
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB;
 
 
 -- Add the roles to the database 
-INSERT INTO user_roles (user_role_name)
+INSERT INTO `user_roles` (user_role_name)
 VALUES ('user'), ('Admin');
 
 -- This is the admin account Password is 123456
-INSERT INTO users (user_id, fullName, username, email,phone, password,gender,active_user,created_at)
-VALUES (1, 'Admin', 'Admin', 'admin@admin.com','555-555-5555','$2b$10$B6yvl4hECXploM.fCDbXz.brkhmgqNlawh9ZwbfkFX.F3xrs.15Xi','male', 1,CURRENT_TIMESTAMP); 
+INSERT INTO `users` (user_id, fullName, username, email, phone, password, gender, active_user, created_at)
+VALUES (1, 'Admin', 'Admin', 'admin@admin.com', '555-555-5555', '$2b$10$B6yvl4hECXploM.fCDbXz.brkhmgqNlawh9ZwbfkFX.F3xrs.15Xi', 'male', 1, CURRENT_TIMESTAMP); 
 
-INSERT INTO user_role (user_id, user_role_id)
+INSERT INTO `user_roles` (user_id, user_role_id)
 VALUES (1, 2); 
