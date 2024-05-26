0b47deab278ca3eb7f99ef7b7a6add448b726a36
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-26 19:00:53 +0300

    ℹ️ Stage and commit: Modified index file

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from '../App';
-import reportWebVitals from './reportWebVitals';
+//index.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "../App";
+import reportWebVitals from "./reportWebVitals";
 // Import the BrowserRouter
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 // Import the AuthContext Provider
-import { AuthProvider } from './components/Contexts/AuthContext';
+import { AuthProvider } from "./components/Contexts/AuthContext";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-      <Provider store={store}>
-        <App />
-        </Provider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
