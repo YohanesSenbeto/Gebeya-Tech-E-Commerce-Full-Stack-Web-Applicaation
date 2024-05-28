00c2341137e0d1e97b72b4a5fddeeb9314173b2c
Yohanes Senbeto Kankure <121575345+YohanesSenbeto@users.noreply.github.com>
2024-05-28 19:07:22 +0000

    ℹ️ Stage and commit: Modified app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,10 @@
 // Import the express module 
 const express = require('express');
+//const mongoose = require("mongoose");
+// const stripe = require("./stripe");
 // Import the dotenv module and call the config method to load the environment variables
 require('dotenv').config();
+const uri = process.env.DB_URI;
 // Import the sanitizer module 
 const sanitize = require('sanitize');
 // Import the CORS module 
@@ -22,6 +25,8 @@ app.use(cors(corsOptions));
 // Add the express.json middleware to the application
 app.use(express.json());
 // Add the sanitizer to the express middleware 
+app.use(cors());
+// app.use("/api/stripe", stripe);
 app.use(sanitize.middleware);
 // Add the routes to the application as middleware 
 app.use(router);
@@ -42,3 +47,11 @@ app.get('/',
 )
 // Export the webserver for use in the application 
 module.exports = app;
+
+/*mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));*/
