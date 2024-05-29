57629527c59728bab4c31a5d321ce272d62334c1
eyobsis <ehtube2020@gmail.com>
2024-05-29 20:31:39 +0300

    ℹ️ Stage and commit: Modified gebeyacart store

--- Frontend/src/components/gebeyacart/App/store.ts
+++ Frontend/src/components/gebeyacart/App/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
+import profileReducer from './profileSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    profile: profileReducer
   },
 });
 
