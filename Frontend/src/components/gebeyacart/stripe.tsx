db2ae622854866eae132087cbcefa38c1e7f2c5d
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-06-04 03:14:20 +0300

    ✨ Update: Modified stripe component

--- Frontend/src/components/gebeyacart/stripe.tsx
+++ Frontend/src/components/gebeyacart/stripe.tsx
@@ -1,6 +1,8 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 // Replace with your own publishable key from Stripe Dashboard
-const stripePromise = loadStripe("pk_test_51OVLc2HjxnpOvxRWl5YIZ6YlWUdabpFjF1hHlMRS0aNUBrY4LdCjd4jTKgvToLjzeuKhKHePgJosQokqtXu9RL8V002dPdJwiw");
+const stripePromise = loadStripe(
+    "pk_test_51OVLc2HjxnpOvxRWl5YIZ6YlWUdabpFjF1hHlMRS0aNUBrY4LdCjd4jTKgvToLjzeuKhKHePgJosQokqtXu9RL8V002dPdJwiw"
+);
 
 export default stripePromise;
