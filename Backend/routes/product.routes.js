fc69452baa77c49f5bc32ab7d959a917310df8af
eyobsis <ehtube2020@gmail.com>
2024-06-05 03:03:27 +0300

    📝 Backend/routes/product.routes.js: updated

--- Backend/routes/product.routes.js
+++ Backend/routes/product.routes.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
-router.get("/api/products/search", productController.searchProducts);
+router.get("/api/search", productController.searchProducts);
+router.get("/api/suggestions", productController.getSuggestions);
 router.post("/api/addProducts",productController.addProducts);
 router.get("/api/getAllProducts", productController.getAllProducts);
 module.exports = router;
