b12efb78f92b27259206934625b21b6603bb7ac2
eyobsis <ehtube2020@gmail.com>
2024-06-05 03:02:41 +0300

    📝 Backend/controllers/Product.Controller.js: updated

--- Backend/controllers/Product.Controller.js
+++ Backend/controllers/Product.Controller.js
@@ -5,21 +5,44 @@ console.log(ProductService)
 
 async function searchProducts(req, res) {
     try {
+        // Extract category and query from request query parameters
         const { category, query } = req.query;
 
-        console.log(category,query)
+        // Log incoming parameters for debugging
+        console.log('Received request to search products. Category:', category, 'Query:', query);
+
+        // Call ProductService to search for products based on category and query
         const products = await ProductService.searchProducts(category, query);
-console.log('serch result')
-        console.log(products)
-        res.status(200).json(
-            products 
-        );
+
+        // Log search results for debugging
+        console.log('Search results:', products);
+
+        // Send the products as JSON response
+        res.status(200).json(products);
     } catch (error) {
-        console.error(error);
+        // Log any errors that occur during the search process
+        console.error('Error occurred while searching for products:', error);
+
+        // Send a 500 Internal Server Error response with a generic error message
         res.status(500).json({ message: 'Internal server error' });
     }
 }
     
+
+
+async function getSuggestions(req, res) {
+    try {
+        const { query } = req.query;
+        console.log(`Suggestions - Query: ${query}`);
+        const suggestions = await ProductService.getSuggestions(query);
+        console.log('Suggestions:', suggestions);
+        res.status(200).json(suggestions);
+    } catch (error) {
+        console.error('Error in getSuggestions:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
 
     async function addProducts(req, res) {
         try {
@@ -52,6 +75,7 @@ console.log('serch result')
 module.exports = {
     searchProducts,
     addProducts,
-    getAllProducts
+    getAllProducts,
+    getSuggestions
 }
 
