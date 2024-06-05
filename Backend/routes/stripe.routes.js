a40550b29b08a0aed6b0e1a2ad0e72a811494958
eyobsis <ehtube2020@gmail.com>
2024-06-05 03:03:37 +0300

    📝 Backend/routes/stripe.routes.js: updated

--- Backend/routes/stripe.routes.js
+++ Backend/routes/stripe.routes.js
@@ -3,14 +3,7 @@ const stripeController = require("../controllers/stripe.controller");
 
 const router = express.Router();
 
-router.post(
-    "/api/stripe/create-checkout-session",
-    stripeController.createCheckoutSession
-);
-router.post(
-    "/webhook",
-    express.json({ type: "application/json" }),
-    stripeController.handleWebhook
-);
+router.post("/api/stripe/create-checkout-session",stripeController.createCheckoutSession);
+router.post("/webhook",express.json({ type: "application/json" }),stripeController.handleWebhook);
 
 module.exports = router;
